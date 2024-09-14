b5a183b7b504a784ef034571725fb84a869d5a22
HuyHoangEngr <alegiahuy11@gmail.com>
2024-09-15 06:05:04 +0700

    #22. Nesting Component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,49 +1,13 @@
-import React from "react";
+import React from "react"
+import UserInfor from "./UserInfor.js";
 
 class MyComponent extends React.Component {
 
-    state = {
-        name: 'HuyHoang',
-        address: 'Dong Nai',
-        age: 30
-    };
-
-    handleMouseOver(event) {
-        // console.log(event.pageX)
-    }
-
-    handleClick(event) {
-        console.log(">> Click me !")
-
-        //merge State => react class
-        this.setState({
-            name: 'Paul',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state)
-    }
     //JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age}
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input
-                        type="text"
-                        onChange={(event) => this.handleOnChangeInput(event)}
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfor />
             </div>
         );
     }
