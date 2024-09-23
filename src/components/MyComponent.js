7b102b2e1f0c3e6856d1d26ee8d52e5719c66ac5
HuyHoangEngr <alegiahuy11@gmail.com>
2024-09-24 06:40:33 +0700

    Giai bai tap Convert Class Component sang su dung Hook

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,60 +1,119 @@
-import React from "react"
+import React, { useState } from "react"
 import AddUserInfor from "./AddUserInfor.js";
 import DisplayInfor from "./DisplayInfor.js";
 
-class MyComponent extends React.Component {
+// class MyComponent extends React.Component {
 
-    state = {
-        listUsers: [
+//     state = {
+//         listUsers: [
+//             { id: 1, name: "Hoi Dan IT", age: "16" },
+//             { id: 2, name: "Eric", age: "26" },
+//             { id: 3, name: "HaryPhamDev", age: "69" },
+//         ]
+//     }
+
+//     handleAddNewUser = (userObj) => {
+//         this.setState({
+//             //let listUsersNew = this.state.listUsers;
+//             //listUsersNew.unshift(userObj);
+//             listUsers: [userObj, ...this.state.listUsers]
+//         })
+//     }
+
+//     handleDeleteUser = (userId) => {
+//         let listUserClone = this.state.listUsers;
+//         listUserClone = listUserClone.filter(item => item.id !== userId);
+//         this.setState({
+//             listUsers: listUserClone
+//         })
+//     }
+
+//     //JSX
+//     render() {
+//         //DRY: don't repeat yourself
+//         //Để check 1 biến thì console.log ngay tai đây
+//         //Không nên in 1 biến trong JSX (trong hàm return phía dưới)
+//         //Nếu có dùng thì dùng {JSON.Stringify(tên biến)}
+//         // console.log(typeof this.state.listUsers)
+//         return (
+//             <>
+//                 < div className='a'>
+//                     <AddUserInfor
+//                         handleAddNewUser={this.handleAddNewUser}
+//                     />
+//                     <br /><br />
+
+//                     <DisplayInfor
+//                         listUsers={this.state.listUsers}
+//                         handleDeleteUser={this.handleDeleteUser}
+//                     />
+//                 </div >
+
+//                 <div className='b'>
+
+//                 </div>
+//             </>
+//         );
+//     }
+// }
+
+const MyComponent = (props) => {
+
+    // state = {
+    //     listUsers: [
+    //         { id: 1, name: "Hoi Dan IT", age: "16" },
+    //         { id: 2, name: "Eric", age: "26" },
+    //         { id: 3, name: "HaryPhamDev", age: "69" },
+    //     ]
+    // }
+
+    const [listUsers, setListUsers] = useState(
+        [
             { id: 1, name: "Hoi Dan IT", age: "16" },
             { id: 2, name: "Eric", age: "26" },
             { id: 3, name: "HaryPhamDev", age: "69" },
         ]
+    )
+
+    const handleAddNewUser = (userObj) => {
+        // this.setState({
+        //     //let listUsersNew = this.state.listUsers;
+        //     //listUsersNew.unshift(userObj);
+        //     listUsers: [userObj, ...this.state.listUsers]
+        // })
+
+        setListUsers([userObj, ...listUsers]);
     }
 
-    handleAddNewUser = (userObj) => {
-        this.setState({
-            //let listUsersNew = this.state.listUsers;
-            //listUsersNew.unshift(userObj);
-            listUsers: [userObj, ...this.state.listUsers]
-        })
-    }
-
-    handleDeleteUser = (userId) => {
-        let listUserClone = this.state.listUsers;
+    const handleDeleteUser = (userId) => {
+        let listUserClone = listUsers;
         listUserClone = listUserClone.filter(item => item.id !== userId);
-        this.setState({
-            listUsers: listUserClone
-        })
+        // this.setState({
+        //     listUsers: listUserClone
+        // })
+
+        setListUsers(listUserClone);
     }
 
-    //JSX
-    render() {
-        //DRY: don't repeat yourself
-        //Để check 1 biến thì console.log ngay tai đây
-        //Không nên in 1 biến trong JSX (trong hàm return phía dưới)
-        //Nếu có dùng thì dùng {JSON.Stringify(tên biến)}
-        // console.log(typeof this.state.listUsers)
-        return (
-            <>
-                < div className='a'>
-                    <AddUserInfor
-                        handleAddNewUser={this.handleAddNewUser}
-                    />
-                    <br /><br />
+    return (
+        <>
+            < div className='a'>
+                <AddUserInfor
+                    handleAddNewUser={handleAddNewUser}
+                />
+                <br /><br />
 
-                    <DisplayInfor
-                        listUsers={this.state.listUsers}
-                        handleDeleteUser={this.handleDeleteUser}
-                    />
-                </div >
+                <DisplayInfor
+                    listUsers={listUsers}
+                    handleDeleteUser={handleDeleteUser}
+                />
+            </div >
 
-                <div className='b'>
+            <div className='b'>
 
-                </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
 }
 
 export default MyComponent;
