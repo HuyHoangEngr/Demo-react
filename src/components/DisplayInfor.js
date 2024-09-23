e67e4f4d6a5b39b170765ff904776c8c5844ff6f
HuyHoangEngr <alegiahuy11@gmail.com>
2024-09-23 07:04:28 +0700

    #33. React Lifecycle - Vong doi ung dung React

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -4,8 +4,29 @@ import logo1 from './../logo.svg'
 
 class DisplayInfor extends React.Component {
 
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log(">>> call constructor: 1")
+        super(props);
+        //babel compiler
+        this.state = {
+            isShowListUser: true
+        }
+    }
+
+    componentDidMount() {
+        console.log(">>> call me component did mount");
+        setTimeout(() => {
+            document.title = 'Eric & Hoi Dan IT';
+        }, 3000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('>>> call me component did update', this.props, prevProps);
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('You got 5 users');
+            }
+        }
     }
 
     handleShowHide = () => {
@@ -15,6 +36,7 @@ class DisplayInfor extends React.Component {
     }
 
     render() {
+        console.log('>>> call me render ')
         //destructuring array/object
         const { listUsers } = this.props;
         //object- dùng destructuring thì phần tử phải giống với phẩn tử ở Component cha
