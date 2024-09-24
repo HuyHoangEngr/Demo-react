4e3d3702f994c69e0dc7b17175a501c962752a58
HuyHoangEngr <alegiahuy11@gmail.com>
2024-09-24 19:39:14 +0700

    #38. useEffect Hook - Su Dung LifeCycle voi React Function Component

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,41 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss'
 import logo1 from './../logo.svg'
 
 //stateless vs stateful
-// class DisplayInfor extends React.Component {
-
-//     render() {
-//         console.log('>>> call me render ')
-//         //destructuring array/object
-//         const { listUsers } = this.props;
-//         //object- dùng destructuring thì phần tử phải giống với phẩn tử ở Component cha
-//         //const listUsers = this.props.listUsers;
-//         //props => viết tắt properties: tài sản cha truyền sang con
-//         return (
-//             <div className='display-info-container'>
-//                 {true &&
-//                     <>
-//                         {listUsers.map((user, index) => {
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
-//                                     <div>
-//                                         <div>My name's {user.name}</div>
-//                                         <div>My age's {user.age}</div>
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                     </div>
-//                                     <hr />
-//                                 </div>
-//                             )
-//                         })}
-//                     </>
-//                 }
-//             </div>
-//         )
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const { listUsers } = props;
@@ -47,6 +14,17 @@ const DisplayInfor = (props) => {
         //Cap nhat lai gia tri cua bien isShowHideListUser
         setShowHideListUser(!isShowHideListUser);
     }
+
+    console.log(">>> call me render")
+
+    useEffect(
+        () => {
+            if (listUsers.length === 0) {
+                alert('You deleted all the users')
+            }
+            console.log('>>> call me useEffect')
+        }, [listUsers]
+    );
 
     return (
         <div className='display-info-container'>
